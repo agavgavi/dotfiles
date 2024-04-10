77d76efc2ad944b9ca21b9de765c9566581420f2
Andrew Gavgavian <andg@odoo.com>
2024-04-10 08:27:19 -0700

    Add file browser

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -132,15 +132,18 @@ local plugins = {
     },
   },
   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
- {
+  {
     "nvim-telescope/telescope.nvim",
     dependencies = {
-      { 
+      {
           "nvim-telescope/telescope-live-grep-args.nvim" ,
           -- This will not install any breaking changes.
           -- For major updates, this must be adjusted manually.
           version = "^1.0.0",
       },
+      {
+        "nvim-telescope/telescope-file-browser.nvim",
+      }
     },
     opts = {
       extensions_list = { "themes", "terms", "live_grep_args", "fzf"},
