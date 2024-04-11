c30d47518c00b9dd51249ca1ef8b159fd1f58cad
Andrew Gavgavian <andg@odoo.com>
2024-04-11 13:09:16 -0700

    improve LSP floating style

--- nvim/configs/lspconfig.lua
+++ nvim/configs/lspconfig.lua
@@ -4,17 +4,32 @@ local on_attach = config.on_attach
 local capabilities = config.capabilities
 
 local lspconfig = require("lspconfig")
-
+local border = {
+    { '┌', 'FloatBorder' },
+    { '─', 'FloatBorder' },
+    { '┐', 'FloatBorder' },
+    { '│', 'FloatBorder' },
+    { '┘', 'FloatBorder' },
+    { '─', 'FloatBorder' },
+    { '└', 'FloatBorder' },
+    { '│', 'FloatBorder' },
+}
 vim.diagnostic.config({
   virtual_text = false,
   signs = true,
   underline = true,
   update_in_insert = false,
   severity_sort = false,
+  float = {border = border},
+  hover = {border = border}
 })
-
+local handlers =  {
+  ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border}),
+  ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border }),
+}
 lspconfig.pyright.setup({
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = {"python"}
+  filetypes = {"python"},
+  handlers = handlers,
 })
